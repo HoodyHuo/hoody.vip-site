1cc1d03f16e91535675f8d3db3dd1285cd9d372b
HoodyHuo <rnmdo@163.com>
2019-07-11 11:27:21 +0800

    clean

--- src/main/groovy/vip/hoody/api/repository/RolePermissionRepository.groovy
+++
@@ -1,9 +0,0 @@
-package vip.hoody.api.repository
-
-import vip.hoody.api.domain.Role
-import vip.hoody.api.domain.RolePermission
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface RolePermissionRepository extends JpaRepository<RolePermission, Long> {
-    public List<RolePermission> findAllByRole(Role role)
-}
--- src/main/groovy/vip/hoody/api/service/CacheService.groovy
+++
@@ -1,9 +0,0 @@
-package vip.hoody.api.service
-
-import org.springframework.stereotype.Service
-
-@Service
-interface CacheService {
-    public List getList()
-    
-}
