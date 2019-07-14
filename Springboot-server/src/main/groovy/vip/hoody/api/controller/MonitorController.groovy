5f110312d2da90743567ff1cdafb4bead4b99cce
HoodyHuo <rnmdo@163.com>
2019-07-15 00:43:18 +0800

    增加用户登录信息 see :https://docs.spring.io/spring/docs/5.1.8.RELEASE/spring-framework-reference/web.html#websocket-stomp-exception-handler

--- Springboot-server/src/main/groovy/vip/hoody/api/controller/MonitorController.groovy
+++ Springboot-server/src/main/groovy/vip/hoody/api/controller/MonitorController.groovy
@@ -1,19 +1,19 @@
 package vip.hoody.api.controller
 
-import org.apache.shiro.SecurityUtils
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.stereotype.Controller
 import vip.hoody.api.util.ResponseData
 import vip.hoody.api.util.WebsocketMessage
 
+import java.security.Principal
+
 @Controller
 class MonitorController {
 
     @MessageMapping("/hello")
     @SendTo("/topic/helloEcho")
-    public ResponseData helloEcho(WebsocketMessage message) throws Exception {
-
-        return new ResponseData(data: SecurityUtils.getSubject().getPrincipal())
+    public ResponseData helloEcho(WebsocketMessage message, Principal principal) throws Exception {
+        return new ResponseData(data: "hello,${principal.name}")
     }
 }
