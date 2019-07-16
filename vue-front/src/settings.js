f8a15450df51842b062981e2b41ec4970030ddc3
HoodyHuo <rnmdo@163.com>
2019-07-16 10:16:09 +0800

    replace ip Address to Domain Name

--- vue-front/src/settings.js
+++ vue-front/src/settings.js
@@ -2,7 +2,7 @@ module.exports = {
 
   title: "Hoody's Site",
 
-  adminHerf: 'http://114.55.171.14/admin',
+  adminHerf: 'http://hoody.vip/admin',
   /**
    * @type {boolean} true | false
    * @description Whether fix the header
