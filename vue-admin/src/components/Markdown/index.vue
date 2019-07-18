09f06b1cfc6287447489e10079c9ac1f20278193
HoodyHuo <rnmdo@163.com>
2019-07-18 23:07:54 +0800

    1.将md渲染html 封装为utils/markdown

--- vue-admin/src/components/Markdown/index.vue
+++ vue-admin/src/components/Markdown/index.vue
@@ -6,26 +6,26 @@ Markdown 显示组件，传入content 作为prop 参数
 
 <template>
   <div class="markdown-body" v-html="htmlContent" />
-
 </template>
 
 <script>
-const marked = require('marked')
-import 'github-markdown-css'
-
+import { getMD2HtmlClean } from '../../utils/markdown'; 'github-markdown-css'
 export default {
   name: 'Markdown',
   props: ['content'],
   data() {
     return {
-      htmlContent: marked(this.$props.content)
+      htmlContent: ''
     }
   },
   watch: {
     content(newVal, oldVal) {
-      this.htmlContent = marked(newVal)
+      this.htmlContent = getMD2HtmlClean(this.$props.content)
     }
+  },
+  methods: {
   }
+
 }
 </script>
 
