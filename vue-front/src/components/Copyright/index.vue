eb066e92e6fb73ae69eab4f97edc201d340fca5c
HoodyHuo <rnmdo@163.com>
2019-07-19 13:32:23 +0800

    增加版权声明组件

---
+++ vue-front/src/components/Copyright/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="copyright">
+    <svg-icon icon-class="copyright" />除非注明，否则均为HoodyHuo原创文章，转载必须以链接形式标明本文链接
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Copyright'
+
+}
+</script>
+
+<style scoped>
+.copyright{
+    background-color: #e0e2e4;
+    padding: 5px;
+    color:#cc4372;
+    font-size: 1.5625rem;
+    font-weight: 500;
+}
+</style>
