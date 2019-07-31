ad73fd6786a1155d50a08adef667cfb3e3cb58d9
HoodyHuo <rnmdo@163.com>
2019-07-31 16:37:04 +0800

    博客图片,添加点击看大图

---
+++ ssr-front/components/BigImage/index.vue
@@ -0,0 +1,88 @@
+
+<template>
+  <!-- 过渡动画 -->
+  <transition name="fade">
+    <div v-show="isShow" class="img-view" @click="closeView">
+      <!-- 遮罩层 -->
+      <div class="img-layer" />
+      <div class="img">
+        <img :src="currentSrc">
+      </div>
+    </div>
+  </transition>
+</template>
+<script>
+// use
+/**
+ * <BigImage ref="img" />
+ * this.$refs.img.show(src)
+ **/
+export default {
+  name: 'BigImage',
+  data() {
+    return {
+      currentSrc: '',
+      isShow: false
+    }
+  },
+  methods: {
+    show(src) {
+      this.currentSrc = src
+      this.isShow = true
+    },
+    closeView() {
+      this.isShow = false
+    }
+  }
+}
+</script>
+<style scoped>
+  /*动画*/
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: all .2s linear;
+    transform: translate3D(0, 0, 0);
+
+  }
+
+  .fade-enter,
+  .fade-leave-active {
+    transform: translate3D(100%, 0, 0);
+
+  }
+
+  /* bigimg */
+  .img-view {
+    position: inherit;
+    width: 100%;
+    height: 100%;
+
+  }
+
+  /*遮罩层样式*/
+  .img-view .img-layer {
+    position: fixed;
+    z-index: 999;
+    top: 0;
+    left: 0;
+    background: rgba(0, 0, 0, 0.7);
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+
+  }
+
+  /*不限制图片大小，实现居中*/
+  .img-view .img img {
+    max-width: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: block;
+    position: fixed;
+    margin: auto;
+    z-index: 1000;
+
+  }
+</style>
--- ssr-front/components/Markdown/index.vue
+++ ssr-front/components/Markdown/index.vue
@@ -5,14 +5,21 @@ Markdown 显示组件，传入content 作为prop 参数
 -->
 
 <template>
-  <div class="markdown-body" v-html="htmlContent" />
+  <div>
+    <div ref="md" class="markdown-body" v-html="htmlContent" />
+    <BigImage ref="img"/>
+  </div>
 </template>
 
 <script>
 import { getMD2HtmlClean } from '../../utils/markdown'
+import BigImage from '../BigImage/'
 
 export default {
   name: 'Markdown',
+  components: {
+    BigImage
+  },
   props: {
     'content': {
       type: String,
@@ -28,10 +35,25 @@ export default {
     htmlContent: function() {
       return getMD2HtmlClean(this.$props.content)
     }
+  },
+  mounted() {
+    const _this = this
+    this.$refs.md.addEventListener('click', function(event) {
+      if (event.srcElement.tagName === 'IMG') {
+        _this.$refs.img.show(event.srcElement.src)
+      }
+    })
+  },
+  methods: {
+    viewImg(src) {
+      this.showImg = true
+      this.imgSrc = src
+    },
+    closeViewImg() {
+      this.showImg = false
+    }
   }
-  // mounted() {
-  //   this.cleanHtml = getMD2HtmlClean(this.$props.content)
-  // }
+
 }
 </script>
 
